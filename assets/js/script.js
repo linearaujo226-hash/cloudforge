26d686459637887c5cbca1332f35f2b7d9dc45f6
LyqqqqneIA <linearaujo226@gmail.com>
2026-07-09 11:52:28 -0300

    Add scroll event listener to change header background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll",()=>{
 
+const header=document.querySelector("header");
+
+if(window.scrollY>30){
+
+header.style.background="#09111f";
+
+}else{
+
+header.style.background="rgba(9,17,31,.85)";
+
+}
+
+});
